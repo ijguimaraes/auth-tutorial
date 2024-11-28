b43cbeeb717f273675c687043c883da65c99923a
Igor José Guimarães <ijguimaraes@gmail.com>
2024-11-28 04:14:09 -0300

    learning how to work with sessions and jwt token in the middeware and implement provider

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,8 @@
 import bcrypt from "bcryptjs"
 import type { NextAuthConfig } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
+import Google from "next-auth/providers/google"
+import Github from "next-auth/providers/github"
 
 import { LoginSchema } from "@/schemas"
 import { getUserByEmail } from "./data/user";
@@ -8,6 +10,8 @@ import { getUserByEmail } from "./data/user";
 
 export default {
     providers: [
+        Google, 
+        Github,
         Credentials({
             async authorize(credentials) {
                 const validatedCredentials = LoginSchema.safeParse(credentials);
--- auth.ts
+++ auth.ts
@@ -1,15 +1,53 @@
-import NextAuth from "next-auth"
+import NextAuth, { type DefaultSession} from "next-auth"
+import { JWT } from "next-auth/jwt"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 
 import { db } from "@/lib/db"
 import authConfig from "@/auth.config"
+import { getUserById } from "./data/user"
+import { UserRole } from "@prisma/client"
+
+declare module "next-auth" {
+    interface Session {
+        user: {
+            role: UserRole
+        } & DefaultSession["user"]
+    }
+}
+ 
+declare module "next-auth/jwt" {
+  interface JWT {
+    role: UserRole
+  }
+}
 
 export const { 
-    handlers: { GET, POST },
+    handlers,
     auth,
     signIn,
     signOut,
 } = NextAuth({
+    callbacks: {
+        async session({token, session}) {
+            if (token.sub && session.user) {
+                session.user.id = token.sub
+            }
+
+            session.user.role = token.role
+            return session
+        },
+        async jwt({ token }) {
+            if (!token.sub) return token
+
+            const existingUser = await getUserById(token.sub)
+
+            if (!existingUser) return token
+
+            token.role = existingUser.role
+
+            return token
+        }
+    },
     adapter: PrismaAdapter(db),
     session: {
         strategy: "jwt",
